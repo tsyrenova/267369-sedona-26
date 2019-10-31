05fddd520095a115df1f33edc398d24c525aaad8
olga <olg_a88@mail.ru>
2019-10-31 23:47:49 +0300

    вносит изменения в соответствии с комментариями наставника по защите

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,6 @@
 var popup = document.querySelector(".search-hotel__popup-form");
 var buttonPopup = document.querySelector(".search-hotel__button");
+popup.classList.remove("search-hotel__popup-form--show")
 
 buttonPopup.addEventListener("click", function(evt) {
     evt.preventDefault();
